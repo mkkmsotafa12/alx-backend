331b8379278e8ece16b61ca895b5a80698000e4d
mkkmsotafa12 <mkkmsotafa12@gmail.com>
2024-06-22 16:01:39 +0300

    0. Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -16,6 +16,8 @@ def index_range(page, page_size):
     Returns:
     tuple: A tuple containing the start index and end index.
     """
+
+    
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
