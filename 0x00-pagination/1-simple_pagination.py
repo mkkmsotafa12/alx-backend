478cc40837551dff7b7c0f7710fd9c555f760b98
mkkmsotafa12 <mkkmsotafa12@gmail.com>
2024-06-22 16:37:15 +0300

    1. Simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -8,31 +8,25 @@ from typing import Tuple, List
 
 
 def index_range(page: int, page_size: int) -> tuple:
-    """
-    Calculate the start and end indices for a given page and page size.
-    
+    """ Calculate the start and end indices for a given page and page size
     Parameters:
     page (int): The current page number (1-indexed).
     page_size (int): The number of items per page.
-    
     Returns:
-    tuple: A tuple containing the start index and end index.
-    """
+    tuple: A tuple containing the start index and end index."""
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """ Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """ Cached dataset """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
